6faab26f47cf2e1b59d797ef5864b0a103fccbd2
toanbui1991 <toanbui1991@gmail.com>
2015-07-22 21:57:36 +0700

    pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,28 +1,39 @@
+# Write a function named 'pollutantmean' that calculates 
+# the mean of a pollutant (sulfate or nitrate) across a specified list of monitors. 
+# The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', 
+# and 'id'. Given a vector monitor ID numbers, 'pollutantmean' reads 
+# that monitors' particulate matter data from the directory specified in the 
+# 'directory' argument and returns the mean of the pollutant across all of the 
+# monitors, ignoring any missing values coded as NA. A prototype of the function 
+# is as follows
+
+# 'directory' is a character vector of length 1 indicating
+# the location of the CSV files
+# 
+# 'pollutant' is a character vector of length 1 indicating
+# the name of the pollutant for which we will calculate the
+# mean; either "sulfate" or "nitrate".
+# 
+# 'id' is an integer vector indicating the monitor ID numbers
+# to be used
+# 
+# Return the mean of the pollutant across all monitors list
+# in the 'id' vector (ignoring NA values)
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+  files <- list.files(directory, full.names=TRUE)
+  dat <- data.frame()
   
-    ## 'directory' is a character vector of length 1 indicating
-    ## the location of the CSV files
-    
-    ## 'pollutant' is a character vector of length 1 indicating
-    ## the name of the pollutant for which we will calculate the
-    ## mean; either "sulfate" or "nitrate".
-    
-    ## 'id' is an integer vector indicating the monitor ID numbers
-    ## to be used
-    
-    ## Returns the mean of the pollutant across all monitors list
-    ## in the 'id' vector (ignoring NA values)
-    
-    files <- (Sys.glob("specdata//*.csv"))[id];
-    
-    combined_data <- c()
-    
-    for (file in files) {
-        file_data <- read.csv(file, sep = ",");
-        pollutant_data <- file_data[,pollutant];
-        pollutant_data <- pollutant_data[!is.na(pollutant_data)]
-        combined_data <- c(combined_data, pollutant_data)
-    }
-    
-    return(mean(combined_data));
+  for(i in id)
+  {
+    dat <- rbind(dat, read.csv(files[i]))
+  }
+  
+  mean_data <- mean(dat[,pollutant], na.rm = TRUE)
+  print(mean_data)
 }
+
+# Unit Test
+# pollutantmean("specdata", "sulfate", 1:10)
+# pollutantmean("specdata", "nitrate", 70:72)
+# pollutantmean("specdata", "nitrate", 23)
